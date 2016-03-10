ac7132651296180806eb7bc5b3de234eeabb3b24
ststeiger <quandary82@hailmail.net>
2016-03-10 15:26:21 +0100

    Added total

--- Embedded2012/Properties/AssemblyInfo.cs
+++ Embedded2012/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Revisions- und Buildnummern 
 // übernehmen, indem Sie "*" wie folgt verwenden:
-[assembly: AssemblyVersion("2016.3.10.1337")]
-[assembly: AssemblyFileVersion("2016.3.10.1337")]
+[assembly: AssemblyVersion("2016.3.10.1524")]
+[assembly: AssemblyFileVersion("2016.3.10.1524")]
